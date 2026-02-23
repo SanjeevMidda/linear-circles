150b06d9d3f0b739d58399272e36322d2d59cd38
Sanjeev Midda <s.midda1@gmail.com>
2026-02-23 20:26:24 +0000

    refactored App.js to map over linear circles instead of hard coding them

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import "./index.css";
 import LinearCircle from "./component/LinearCircle";
+import COLORS from "./data/Colors";
 
 function App() {
   // 4 circles with different colors which rotate in different directions
 
   // creat App.js UI
   // add title
+
+  let noOfLinearCircles = new Array(5).fill(0);
+
+  console.log(noOfLinearCircles);
+
   return (
     <div className="App">
       <h4>music.</h4>
       <div className="linearCirclesContainer">
-        <LinearCircle />
-        <LinearCircle />
-        <LinearCircle />
-        <LinearCircle />
+        {noOfLinearCircles.map((i) => (
+          <LinearCircle />
+        ))}
       </div>
     </div>
   );
