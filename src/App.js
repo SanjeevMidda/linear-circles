6df95e90f6c8f6f2414ddaa020593ec76e8a715b
Sanjeev Midda <s.midda1@gmail.com>
2026-02-23 20:14:05 +0000

    created App.js UI to hold linerar circles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./index.css";
+import LinearCircle from "./component/LinearCircle";
 
 function App() {
-  return <div className="App"></div>;
+  // 4 circles with different colors which rotate in different directions
+
+  // creat App.js UI
+  // add title
+  return (
+    <div className="App">
+      <h4>music.</h4>
+      <div className="linearCirclesContainer">
+        <LinearCircle />
+        <LinearCircle />
+        <LinearCircle />
+        <LinearCircle />
+      </div>
+    </div>
+  );
 }
 
 export default App;
